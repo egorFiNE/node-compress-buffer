1a94b3bda7c4166e3359d20bf888ddf5d7d54cae
Egor Egorov <me@egorfine.com>
2012-05-27 18:16:33 +0300

    no need to support vintage node.js versions.

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-try {
-	module.exports = require(__dirname+'/build/default/compress-buffer-bindings');
-} catch(e) {
-	module.exports = require(__dirname+'/build/Release/compress-buffer-bindings');
-}
-
+module.exports = require(__dirname+'/build/Release/compress-buffer-bindings');
